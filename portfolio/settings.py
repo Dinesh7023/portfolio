5258ddc05a9fbc3f4b9777ea264b7a09780a914d
Dinesh7023 <dk68sdk@outlook.com>
2026-01-01 16:23:04 +0530

    modified

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ['.onrender.com','localhost', '127.0.0.1']
 
 
 # Application definition
